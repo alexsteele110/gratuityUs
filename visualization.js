40ffafa01c58dcff856012cf8df2324a064cf1dc
Alexander Steele <alexsteele110@gmail.com>
2018-01-27 14:56:26 -0500

    refactored data structure for borders and regions

--- visualization.js
+++ visualization.js
@@ -18,9 +18,9 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
   };
 
   const percent = {
-    thirteen: ['48', '40', '05', '22'],
-    fifteen: ['01', '21', '28', '47', '12', '13', '45', '37', '51', '54', '24', '10', '55', '26', '39', '18', '17', '48', '40', '05', '22', '20', '29', '31', '19', '46', '38', '27', '30', '16', '56', '04', '08', '35', '49', '32', '52', '41', '06', '02', '15', '53'],
-    sixteen: ['09', '44', '25', '50', '33', '23', '36', '42', '34']
+    thirteen: regions.westSouthCentral,
+    fifteen: [...regions.eastSouthCentral, ...regions.southAtlantic, ...regions.eastNorthCentral, ...regions.midWest, ...regions.mountain, ...regions.pacific],
+    sixteen: [...regions.newEngland, ...regions.middleAtlantic]
   };
 
   function chooseFill(d) {
@@ -49,15 +49,12 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
       .attr("stroke-width", 0.5)
       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 
-  const pacificBorders = [
+  const borderPairs = [
     ['06', '32'],
     ['06', '04'],
     ['32', '41'],
     ['16', '41'],
-    ['16', '53']
-  ];
-
-  const mountainBorders = [
+    ['16', '53'],
     ['30', '38'],
     ['30', '46'],
     ['56', '46'],
@@ -66,10 +63,7 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     ['08', '20'],
     ['08', '40'],
     ['35', '40'],
-    ['35', '48']
-  ];
-
-  const midwestBorders = [
+    ['35', '48'],
     ['27', '55'],
     ['19', '55'],
     ['19', '17'],
@@ -78,29 +72,17 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     ['29', '47'],
     ['29', '05'],
     ['20', '40'],
-    ['40', '29']
-  ];
-
-  const wsCentralBorders = [
+    ['40', '29'],
     ['05', '47'],
     ['05', '28'],
-    ['22', '28']
-  ];
-
-  const newenglandBorders = [
+    ['22', '28'],
     ['36', '50'],
     ['36', '25'],
-    ['36', '09']
-  ];
-
-  const midAtlanticBorders = [
+    ['36', '09'],
     ['42', '39'],
     ['42', '54'],
     ['42', '24'],
-    ['42', '10']
-  ];
-
-  const esCentralBorders = [
+    ['42', '10'],
     ['01', '12'],
     ['01', '13'],
     ['47', '13'],
@@ -110,10 +92,7 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     ['21', '54'],
     ['21', '39'],
     ['21', '18'],
-    ['21', '17']
-  ];
-
-  const southAtlanticBorders = [
+    ['21', '17'],
     ['39', '54']
   ];
 
@@ -124,59 +103,10 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     };
   };
 
-  pacificBorders.forEach(pair => {
+  borderPairs.forEach(pair => {
     return svg.append("path")
         .attr("stroke", "#fff")
         .attr("stroke-width", 4)
         .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
   });
-
-  mountainBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  midwestBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  wsCentralBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  newenglandBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  midAtlanticBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  esCentralBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-
-  southAtlanticBorders.forEach(pair => {
-    return svg.append("path")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 4)
-        .attr("d", path(topojson.mesh(us, us.objects.states, border(pair[0], pair[1]))))
-  });
-})
+});
