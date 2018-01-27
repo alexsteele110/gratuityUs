26d14571d8d0605786a2739c64098a018a067983
Alexander Steele <alexsteele110@gmail.com>
2018-01-27 13:26:43 -0500

    able to highlight state borders to show regions

--- visualization.js
+++ visualization.js
@@ -1,31 +1,58 @@
-const svg = d3
-  .select('#map')
-  .append('svg')
-  // next two attributes make svg responsive
-  .attr('preserveAspectRatio', 'xMinYMin meet')
-  .attr('viewBox', '0 0 600 400');
+var svg = d3.select("svg");
 
-const projection = d3.geoAlbersUsa().scale(660).translate([300, 200]);
+var path = d3.geoPath();
 
-const path = d3.geoPath().projection(projection);
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
 
-d3
-  .queue()
-  .defer(d3.json, 'us.json')
-  .await(ready);
+  svg.append("g")
+      .attr("fill", "#000")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr('class', 'states')
+      .attr("d", path)
+      .style('fill', '#008592')
+    .append("title")
+      .text(function(d) { return d.id; });
 
-  function ready(error, us) {
-    if (error) throw error;
-    console.log(us);
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 0.5)
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 
-    d3.json('us.json', us => {
-      svg
-        .selectAll('.states')
-        .data(topojson.feature(us, us.objects.usStates).features)
-        .enter()
-        .append('path')
-        .attr('class', 'states')
-        .attr('d', path)
-        .style('fill', '#008592');
-    });
-  }
+  // California-Nevada border
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 4)
+      .attr("d", path(topojson.mesh(us, us.objects.states, border("06", "32"))));
+
+  // California-Arizona border
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 4)
+      .attr("d", path(topojson.mesh(us, us.objects.states, border("06", "04"))));
+
+  // Nevada-Oregon border
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 4)
+      .attr("d", path(topojson.mesh(us, us.objects.states, border("32", "41"))));
+
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 4)
+      .attr("d", path(topojson.mesh(us, us.objects.states, border("16", "41"))));
+
+  svg.append("path")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 4)
+      .attr("d", path(topojson.mesh(us, us.objects.states, border("16", "53"))));
+});
+
+function border(id0, id1) {
+  return function(a, b) {
+    return a.id === id0 && b.id === id1
+        || a.id === id1 && b.id === id0;
+  };
+}
